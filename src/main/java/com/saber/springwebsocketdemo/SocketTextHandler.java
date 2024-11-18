6687cfc4b74b813ad53bbee515398e4618b8fd00
azizi.s <aziz.s@tiddev.com>
2024-11-18 09:51:47 +0330

    add save sessions

--- src/main/java/com/saber/springwebsocketdemo/SocketTextHandler.java
+++ src/main/java/com/saber/springwebsocketdemo/SocketTextHandler.java
@@ -1,21 +1,33 @@
 package com.saber.springwebsocketdemo;
 
+import jakarta.websocket.Session;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class SocketTextHandler extends TextWebSocketHandler {
+
+    private List<WebSocketSession> sessions = new ArrayList<>();
+
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        System.out.println("session id "+session.getId()+" connected");
+        System.out.println("session id " + session.getId() + " connected");
+        sessions.add(session);
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        super.handleTextMessage(session,message);
+        super.handleTextMessage(session, message);
         String payload = message.getPayload();
-        System.out.printf("session %s send message %s%n", session.getId(),payload);
-        session.sendMessage(new TextMessage(String.format("thank you your message is %s",payload)));
+        System.out.printf("session %s send message %s%n", session.getId(), payload);
+        sessions = sessions.stream().filter(s -> s.getId().equals(session.getId())).toList();
+        for (WebSocketSession webSocketSession : sessions) {
+            webSocketSession.sendMessage(new TextMessage(String.format("thank you your message is %s", payload)));
+        }
     }
 }
